d8d45372434486fbeb313722d74af60bfd59db4d
Ram Kaushik <ramkaushik44@gmail.com>
2024-09-08 18:14:30 +0530

    Added bootstrap cdn and modified search form style

--- resources/views/search/form.blade.php
+++ resources/views/search/form.blade.php
@@ -2,27 +2,40 @@
 <html>
 <head>
     <title>Search Form</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <h1>Search Form</h1>
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-    <form action="{{ route('search.handle') }}" method="POST">
-        @csrf
-        @for ($i = 0; $i < 5; $i++)
-            <div>
-                <label for="query{{ $i }}">Search Query {{ $i + 1 }}:</label>
-                <input type="text" name="queries[]" id="query{{ $i }}">
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="text-center">Search Form</h4>
+                </div>
+                <div class="card-body">
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+                <form action="{{ route('search.handle') }}" method="POST">
+                    @csrf
+                    @for ($i = 0; $i < 5; $i++)
+                        <div>
+                            <label for="query{{ $i }}">Search Query {{ $i + 1 }}:</label>
+                            <input type="text" name="queries[]" id="query{{ $i }}">
+                        </div>
+                    @endfor
+                    <button type="submit">Search</button>
+                </form>
+                </div>
             </div>
-        @endfor
-        <button type="submit">Search</button>
-    </form>
+        </div>
+    </div>
+</div>
 </body>
 </html>
